d5db110c5123e89c059cb2cf7c703c0ae28e8f75
KEITHALFAROEC7C\alfarok <keith4895@gmail.com>
2017-12-18 15:47:14 -0500

    dynamo talking w/ VP

--- src/RechargeViewExtension/LiteVP/LiteVP.cs
+++ src/RechargeViewExtension/LiteVP/LiteVP.cs
@@ -5,31 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 using Dynamo;
 using Autodesk.DesignScript.Geometry;
-using RechargeViewExtension;
 using Newtonsoft.Json.Linq;
 
 namespace LiteVP
 {
     public class LiteVP
     {
-        public static string BuildMesh(string name, Vector[] vertices, int[] vertexIndicesbyTri)
+        public static string BuildMesh(string name, double[] vertices, int[] vertexIndicesbyTri)
         {
             // Build json object
-            List<double> meshVertices = new List<double>();
-            
-            for (int i = 0; i < vertices.Length; i += 3)
-            {
-                meshVertices.Add(vertices[i].X);
-                meshVertices.Add(vertices[i].Y);
-                meshVertices.Add(vertices[i].Z);
-            }
-
             JObject meshObject = new JObject(
                 new JProperty("name", name),
-                new JProperty("vertices", meshVertices),
+                new JProperty("vertices", vertices),
                 new JProperty("faceIndices", vertexIndicesbyTri));
-
-            string jsonString = meshObject.ToString();
 
             // Viewer should identify this node is active via listening for an event
 
@@ -38,6 +26,8 @@ namespace LiteVP
             // We call a new JS function that builds or updates the mesh
 
             // return string representation
+            string jsonString = meshObject.ToString();
+
             return jsonString;
         }
     }
--- src/RechargeViewExtension/RechargeViewExtension/RechargeWindowViewModel.cs
+++ src/RechargeViewExtension/RechargeViewExtension/RechargeWindowViewModel.cs
@@ -2,6 +2,7 @@
 using Dynamo.Core;
 using Dynamo.Extensions;
 using Dynamo.Graph.Nodes;
+using Dynamo.Graph.Workspaces;
 using CefSharp;
 
 namespace RechargeViewExtension
@@ -13,23 +14,29 @@ namespace RechargeViewExtension
         // Variable for storing a reference to our loaded parameters
         private ReadyParams readyParams;
 
-        // Display the number of nodes in the active workspace
-        public string SelectedNodesText => $"Disabled";
+        // Find render nodes and build THREE meshes
+        public string SelectedNodesText => $"{getNodeTypes()}";
 
-        /*
         public string getNodeTypes()
         {
-            string output = "";
+            string output = "buildMeshFromJson(";
 
             foreach (NodeModel node in readyParams.CurrentWorkspaceModel.Nodes)
             {
-                string nickName = node.NickName;
-                output += nickName + "\n";
+                string nickName = node.Name;
+
+                if(nickName == "LiteVP.BuildMesh")
+                {
+                    // TODO - need to check if this is a list or not
+                    // in order to properly handle replication
+                    output += node.CachedValue.Data.ToString();
+                }
             }
+
+            output += ");";
 
             return output;
         }
-        */
 
         public RechargeWindowViewModel(ReadyParams p)
         {
@@ -40,10 +47,19 @@ namespace RechargeViewExtension
             // Subscribe to NodeAdded and NodeRemoved events
             p.CurrentWorkspaceModel.NodeAdded += CurrentWorkspaceModel_NodesChanged;
             p.CurrentWorkspaceModel.NodeRemoved += CurrentWorkspaceModel_NodesChanged;
+
+            //p.CurrentWorkspaceChanged += CurrentWorkspaceModel_NodesChanged;
         }
 
         // Define what happens when the event above if triggered
         private void CurrentWorkspaceModel_NodesChanged(NodeModel obj)
+        {
+            // Event used to notify UI or bound elements that
+            // the data has changed and needs to be updated
+            RaisePropertyChanged("SelectedNodesText");
+        }
+
+        private void CurrentWorkspaceModel_NodesChanged(IWorkspaceModel obj)
         {
             // Event used to notify UI or bound elements that
             // the data has changed and needs to be updated
