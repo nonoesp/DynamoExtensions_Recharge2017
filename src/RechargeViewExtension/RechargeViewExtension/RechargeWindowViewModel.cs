1b22cb38f8b64d34f059bfa201c87c3f93e143bd
KEITHALFAROEC7C\alfarok <keith4895@gmail.com>
2017-12-22 10:49:32 -0500

    reading OOTB solids

--- src/RechargeViewExtension/RechargeViewExtension/RechargeWindowViewModel.cs
+++ src/RechargeViewExtension/RechargeViewExtension/RechargeWindowViewModel.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Autodesk.DesignScript.Interfaces;
 using Dynamo.Core;
 using Dynamo.Extensions;
 using Dynamo.Graph.Nodes;
 using Dynamo.Graph.Workspaces;
 using Dynamo.Models;
+using Dynamo.Visualization;
 using CefSharp;
+using Newtonsoft.Json.Linq;
 
 namespace RechargeViewExtension
 {
@@ -14,6 +19,8 @@ namespace RechargeViewExtension
 
         // Variable for storing a reference to our loaded parameters
         private ReadyParams readyParams;
+
+        public RenderPackageCache packageContent;
 
         // Find render nodes and build THREE meshes
         public string SelectedNodesText => $"{getNodeTypes()}";
@@ -25,17 +32,45 @@ namespace RechargeViewExtension
             // Mesh must be set similar to 'geometry.verticesNeedUpdate = true;'
             string output = "scene.remove(mesh);\n renderDynamoMesh(";
 
+            /*
             foreach (NodeModel node in readyParams.CurrentWorkspaceModel.Nodes)
             {
                 string nickName = node.Name;
 
+                
                 if(nickName == "MinimalVP.RenderMesh")
                 {
                     // TODO - need to check if this is a list or not
                     // in order to properly handle replication
                     output += node.CachedValue.Data.ToString();
                 }
+                
             }
+        */
+
+            // TODO don't convert enums to lists
+            List<double> verts = new List<double>();
+            List<int> indices = new List<int>();
+            List<double> norms = new List<double>();
+
+            foreach (IRenderPackage p in packageContent.Packages)
+            {
+                verts = p.MeshVertices.ToList();
+                indices = p.MeshIndices.ToList();
+                norms = p.MeshNormals.ToList();
+            }
+
+            JObject meshObject = new JObject(
+                new JProperty("name", "newMesh"),
+                new JProperty("vertices", verts),
+                new JProperty("faceIndices", indices));
+
+            // How do I inject meshObject into view extension
+            string jsonString = meshObject.ToString();
+
+            output += jsonString;
+
+            System.IO.File.WriteAllText(@"C:\Users\alfarok\Desktop\mesh.txt", jsonString);
 
             output += ");";
 
@@ -49,12 +84,24 @@ namespace RechargeViewExtension
             readyParams = p;
 
             // Subscribe to NodeAdded and NodeRemoved events
-            p.CurrentWorkspaceModel.NodeAdded += CurrentWorkspaceModel_NodesChanged;
-            p.CurrentWorkspaceModel.NodeRemoved += CurrentWorkspaceModel_NodesChanged;
+            //p.CurrentWorkspaceModel.NodeAdded += CurrentWorkspaceModel_NodesChanged;
+            //p.CurrentWorkspaceModel.NodeRemoved += CurrentWorkspaceModel_NodesChanged;
 
             // TODO this could be dangerous if called in custom node ws
             var currentWS = p.CurrentWorkspaceModel as HomeWorkspaceModel;
-            currentWS.RefreshCompleted += CurrentWorkspaceModel_NodesChanged;
+            //currentWS.RefreshCompleted += CurrentWorkspaceModel_NodesChanged;
+
+            foreach (NodeModel node in currentWS.Nodes)
+            {
+                if (node.Name == "Cuboid.ByLengths")
+                {
+                    node.RenderPackagesUpdated += CurrentWorkspaceModel_CuboidReady;
+                }
+                else if (node.Name == "Sphere.ByCenterPointRadius")
+                {
+                    node.RenderPackagesUpdated += CurrentWorkspaceModel_CuboidReady;
+                }
+            }
         }
 
         // Define what happens when the event above if triggered
@@ -72,11 +119,24 @@ namespace RechargeViewExtension
             RaisePropertyChanged("SelectedNodesText");
         }
 
+        private void CurrentWorkspaceModel_CuboidReady(NodeModel nodeModel, RenderPackageCache packages)
+        {
+            packageContent = packages;
+            // Event used to notify UI or bound elements that
+            // the data has changed and needs to be updated
+            RaisePropertyChanged("SelectedNodesText");
+        }
+
         // Very important - unsubscribe from our events to prevent a memory leak
         public void Dispose()
         {
+            /*
             readyParams.CurrentWorkspaceModel.NodeAdded -= CurrentWorkspaceModel_NodesChanged;
             readyParams.CurrentWorkspaceModel.NodeRemoved -= CurrentWorkspaceModel_NodesChanged;
+
+            var currentWS = readyParams.CurrentWorkspaceModel as HomeWorkspaceModel;
+            currentWS.RefreshCompleted -= CurrentWorkspaceModel_NodesChanged;
+            */
         }
     }
 }
